3846632f9ffbdce0f8d60536959a009f7ce9d9ab
okE1y <usanov447@gmail.com>
2025-10-04 02:26:37 +0300

    Game cycle maket

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<Sequence> sequences = new List<Sequence>();
+
+    private bool playerEndMove = false;
     private void Awake()
     {
         Game.GameManager = this;
@@ -15,12 +17,47 @@ public class GameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(1f);
 
-        GameObject car = GameObject.Find("FirstCar");
-        car.GetComponent<IntPos>().CreateChangePosContext(car.GetComponent<IntPos>().pos, new Vector2Int(5, 1));
-        car.GetComponent<IntPos>().CreateChangePosContext(new Vector2Int(5, 1), new Vector2Int(8, 3));
 
-
-        car.GetComponent<EntityRenderer>().ChangePosition();
-        car.GetComponent<IntPos>().ApplyMove();
+        while (true) // игровой цикл
+        {
+            yield return PlayerMovePhase();
+            yield return GamePhase();
+        }
     }
+
+    private IEnumerator PlayerMovePhase()
+    {
+        // Вызов интерфейса
+        // передача управления над камерой
+
+        yield return new WaitUntil(() => playerEndMove); // ждем когда игрок закончит ход
+        yield break;
+    }
+
+    private IEnumerator GamePhase()
+    {
+        do
+        {
+            MakeRoutePhase();
+            yield return ApplyMovePhase();
+        }while (!CheckFinishCondition());
+    }
+
+    private void MakeRoutePhase()
+    {
+        //Строим маршруты всех машинок
+    }
+
+    private IEnumerator ApplyMovePhase()
+    {
+        // Воплощяем ходы, собираем ловушки и активируем эффекты
+        yield break;
+    }
+
+    private bool CheckFinishCondition()
+    {
+        // Проверяем условие завершения хода
+        return true;
+    }
+
 }
